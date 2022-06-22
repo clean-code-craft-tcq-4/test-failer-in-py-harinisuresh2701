80fe19f00c47f28dba70a03ea7954354e2ac40c2
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-06-22 18:06:31 +0530

    Update Test_misaligned.py

--- Test_misaligned.py
+++ Test_misaligned.py
@@ -2,5 +2,5 @@ import misaligned
 result, final_string = misaligned.create_printable_string()
 misaligned.print_color_map_on_console(final_string)
 assert(result == 25)
-assert(formatter(10, "Black", "Blue").find("|") == formatter(0, "White" ,"Blue"))
+assert(misaligned.formatter(10, "Black", "Blue").find("|") == misaligned.formatter(0, "White" ,"Blue"))
 print("All is well (maybe!)\n")
