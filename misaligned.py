f5f640602394fb02394bffa0999f87889e21b051
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-06-22 14:21:24 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,17 +1,20 @@
-
-def create_printable_string(print_color_map_on_console()):
+printable_string = []
+final_string = []
+def create_printable_string():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            printable_string = formatter(i * 5 + j, major, minor)
+            printable_string.append(formatter(i * 5 + j, major, minor))
     return len(major_colors) * len(minor_colors), printable_string
 def formatter(pairNum, Major, Minor):
     formatted_string = f'{pairNum} | {Major} | {Minor}'
     return formatted_string
 def print_color_map_on_console(String):
-    print (String)
-result, final_string = print_color_map_on_console()
+    for i in String:
+        print (i,"\n")
+result, final_string = create_printable_string()
+print_color_map_on_console(final_string)
 assert(result == 25)
 assert(formatter(10, "Black", "Blue").find("|") == formatter(0, "White" ,"Blue"))
 print("All is well (maybe!)\n")
