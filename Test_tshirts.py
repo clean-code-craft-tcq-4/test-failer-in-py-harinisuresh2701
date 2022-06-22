bece287c4045c9c34c0cc7a0a08f2338164096ec
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-06-22 12:34:54 +0530

    Update Test_tshirts.py

--- Test_tshirts.py
+++ Test_tshirts.py
@@ -1,7 +1,7 @@
 import tshirts
 #Testing all the boundary conditions
-assert(tshirts.size(35) == 'S')
-assert(tshirts.size(38) == 'S')
+assert(tshirts.size(35) == 'M')
+assert(tshirts.size(38) == 'M')
 assert(tshirts.size(40) == 'M')
 assert(tshirts.size(43) == 'L')
 print("All is well (maybe!)\n")
