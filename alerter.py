041a319be77290699605ea8a637850362b954ec1
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-06-22 18:03:40 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -22,8 +22,8 @@ def alert_in_celcius(farenheit):
         global alert_failure_count
         alert_failure_count += 0
         
-alerter.alert_in_celcius(400.5)
-alerter.alert_in_celcius(303.6)
+alert_in_celcius(400.5)
+alert_in_celcius(303.6)
 assert alert_failure_count == 2
 print(f'{alert_failure_count} alerts failed.')
 print('All is well (maybe!)')
