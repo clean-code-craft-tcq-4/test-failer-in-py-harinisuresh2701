18aeef56baff6077b3381c5ebfaa5f878b2c5b78
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-06-30 11:25:11 +0530

    Update alerter.py

--- alerter.py
+++ alerter.py
@@ -29,9 +29,3 @@ def alert_in_celcius(farenheit,network_alerter_code):
         # Add a test below to catch this bug. Alter the stub above, if needed.
         global alert_failure_count
         alert_failure_count += 1
-        
-alert_in_celcius(400.5,network_alert_stub)
-alert_in_celcius(303.6,network_alert_stub)
-assert alert_failure_count == 2
-print(f'{alert_failure_count} alerts failed.')
-print('All is well (maybe!)')
